34412aed0c6ff5495326d778ad36ed168dee269e
luizmarcello <luizmarcello.vmo@hotmail.com>
2023-08-27 19:10:15 -0300

    londrina:27.08.23_19:10hs

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Tweet;
 use Livewire\Component;
 
 class ShowTweets extends Component
 {
-    public $message = 'Apenas um teste';
+    //public $message = 'Apenas um teste';
 
     public function render()
     {
-        return view('livewire.show-tweets');
+        /* with(): Qual relacionamento(o tweet com seu proprietário)
+           estará usando, senão, para cada tweet, fará uma pesquisa no bd */
+        $tweets = Tweet::with('user')->get();
+
+        //return view('livewire.show-tweets', compact('tweetsss'));
+        //ou assim, usando um array
+        return view('livewire.show-tweets', [
+            'tweets' => $tweets
+        ]);
     }
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,10 +1,16 @@
 <div>
-    Show Tweets
+   {{--  Show Tweets --}}
 
     {{-- Imprimindo esta propriedade, do controller "ShowTweets" --}}
-    <p>{{ $message }}</p>
+    {{-- <p>{{ $message }}</p> --}}
 
     {{-- Fazendo um bind(de mão-dupla) do conteúdo deste input, com a propriedade "$message,
          do controller ShowTweets" --}}
-    <input type="text" name="message" id="message" wire:model="message">
+    {{-- <input type="text" name="message" id="message" wire:model="message"> --}}
+
+    @foreach ($tweets as $tweet)
+        {{-- Esse "user" vem do relacionamento "muitos-para-um", do model
+         "Tweet.php", que é proprietário do "tweet" --}}
+        {{ $tweet->user->name }} - {{ $tweet->content }} <br>
+    @endforeach
 </div>
