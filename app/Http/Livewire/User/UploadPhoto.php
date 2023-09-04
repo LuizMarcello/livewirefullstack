a2efd4663b91c5096289935f6c32bf945449dc5d
luizmarcello <luizmarcello.vmo@hotmail.com>
2023-09-04 09:45:44 -0300

    Londrina:04.09.23_09:45hs

--- app/Http/Livewire/User/UploadPhoto.php
+++ app/Http/Livewire/User/UploadPhoto.php
@@ -3,11 +3,25 @@
 namespace App\Http\Livewire\User;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class UploadPhoto extends Component
 {
+    use WithFileUploads;
+
+    public $photooo;
+
     public function render()
     {
         return view('livewire.user.upload-photo');
+    }
+
+    public function storagePhoto()
+    {
+        $this->validate([
+            'photooo' => 'required|image|max:1024'
+        ]);
+
+        dd($this->photooo);
     }
 }
--- resources/views/livewire/user/upload-photo.blade.php
+++ resources/views/livewire/user/upload-photo.blade.php
@@ -1,3 +1,17 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <h2>Atualizar a foto do perfil</h2>
+
+    {{-- wire:submit.prevent: Fazendo um "bind" com o action storagePhoto()
+         no controller(componente) UploadPhoto --}}
+    <form action="#" method="POST" wire:submit.prevent="storagePhoto">
+        {{-- wire:submit.prevent: Enviando o conteúdo(fazendo bind) deste input para a
+             propriedade "$photooo", no controller(componente) "UploadPhoto" --}}
+        <input type="file" name="" id="" wire:model="photooo">
+        {{-- Diretiva do blade para êrros --}}
+        @error('photooo')
+            {{ $message }}
+        @enderror
+        <br>
+        <button type="submit">Upload Foto</button>
+    </form>
 </div>
